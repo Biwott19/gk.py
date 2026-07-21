fe61e0d5e9ba6cd2c4c956bfbc21778e197bd5bc
gilion <gillionbiwott@gmail.com>
2026-07-21 19:46:19 +0300

    complete day 4 assignment

---
+++ day04_assignment.py
@@ -0,0 +1,50 @@
+first_name='gilion' 
+last_name='biwott' 
+age = 24
+height = 176.5
+course = 'computer science' 
+university = 'Moi university' 
+year_of_study = 3.5
+favorite_language ='python'
+dream_company ='open ai'
+dream_job ='ai engineer'
+num_siblings = 3
+own_laptop = True
+has_experience = False
+#STEP 2 : DISPLAYING THE PROFILE
+print('======STUDENT PROFILE======')
+print(f'name:{first_name}  {last_name }')
+print(f'age:{age}')
+print(f'heigh: {height}m')
+print(f'coursre:{course}')
+print(f'university:{university}')
+print(f'year of study:{year_of_study}')
+print(f'favorite language:{favorite_language}')
+print(f'dream company:{dream_company}')
+print(f'dream job:{dream_job}')
+print(f'number of siblings:{num_siblings}')
+print(f'owns laptop:{own_laptop}')
+print(f'programming experience:{has_experience}')
+print('=============================/n')
+#STEP 3: INSPECTING THE DATA TYPES
+print('=========DATA TYPES INSPECTION=========')
+print(f'name: {type(first_name)}')
+print(f'age: {type(age)}')
+print(f'height: {type(height)}')
+print(f'course: {type(course)}')
+print(f'university: {type(university)}')
+print(f'year of study: {type(year_of_study)}')
+print(f'favorite language: {type(favorite_language)}')
+print(f'dream company: {type(dream_company)}')
+print(f'dream job: {type(dream_job)}')
+print(f'number of siblings: {type(num_siblings)}')
+print(f'owns laptop: {type(own_laptop)}')
+print(f'has experience: {type(has_experience)}')
+print('======================================')
+#STEP4:PREDICTIONS
+print('==========STEP 4: PREDICTIONS RESULTS==========')
+print(type('100'))
+print(type(100))
+print(type(100.5))
+print(type(False))
+print('================================\n')
