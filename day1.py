9bdd640618f04a13d69b96874b45c5d10f38e57f
gilion <gillionbiwott@gmail.com>
2026-07-22 14:13:37 +0300

    add Day 1 concept review

---
+++ day1.py
@@ -0,0 +1,21 @@
+print ('hello world')
+#We have 4 data types in python
+#1. String  man,boy,gilionbiwott@gmail.com
+#2. Integer  25, 30, 35
+#3. Float  25.5, 30.5, 35.5
+#4. Boolean  True, False 1, 0
+print('my name is gilion biwott and i am 25 years old')
+print('i am a student at moi university')
+#VARIABLES
+#Containers for data storage
+age = 25
+print(age)
+print('my name is gilion biwott and i am ' + str(age) + ' years old')
+print('my name is gilion biwott and i am ' , age , 'years old')
+print('my name is gilion biwott and i am {age} years old')
+#SYNTAX
+if 5 > 10:
+    print('false')
+else:
+    print('true')
+        
