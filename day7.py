1f89d953495042ed7b15ee9f6c9fe73b4287fa6e
gilion <gillionbiwott@gmail.com>
2026-07-23 21:03:31 +0300

    complete day 7 assignment :break and continue loops

---
+++ day7.py
@@ -0,0 +1,51 @@
+#loops
+print("Today is on a thursday")
+#we want to print this 100 tims
+#types of loops
+#whule loops
+#for loops
+
+# while loops 
+  #code
+#cake = 1
+#while cake <= 9:
+ # print(cake)
+  #for loops 
+#repeat over sequence
+for numbers in range (7):
+   print (numbers)
+#range 
+#range (5) prints 0 - 4
+for i in range (5):
+   print(1)
+for i in range (0,12,4):
+   print(i)
+#looping through strings
+for letters in "PYTHON":
+   print(letters)
+#break 
+#stps the loop immediately
+   for i in range (1,11):
+    if 1==6:
+      break
+   print (i)
+   #continue 
+   #skips one iteration
+
+   for i in range (1,11):
+      if i -- 5:
+         continue
+      print(i)
+
+
+#password checker
+password = ""
+while password !="12345":
+    password = input ("enter your password: ")
+
+print( "WELCOME" )
+
+
+
+    
+ 
