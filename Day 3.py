cecba1000eaec06290ed7eac1f9ead9353556c66
gilion <gillionbiwott@gmail.com>
2026-07-23 07:50:39 +0300

    complete day 3 assignment

---
+++ Day 3.py
@@ -0,0 +1,9 @@
+#TERMINAL & BASIC PRINTING
+print('welcome to coding time')
+print('Today i learned the terminal')
+print('i can non run python from the command line.')
+print('-------------------------------------------\n')
+#ADDING OUR CLEAN TRACKING FOOTER
+print('========== ASSINGMENT CHECK==========')
+print('File  "day3.py" executed successfully')
+print('=====================================')
